c142473a56c7cba0e51915eb7cdfde6be9f1f438
Brandon Gonzalez <bg@lightstep.com>
2017-03-08 17:31:48 -0800

    Create HAProxyHTTPLog, add docs, shuffle things into their own functions

--- haproxy-log2span.go
+++ haproxy-log2span.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 
 	"github.com/lightstep/haproxy_log2span/lib"
 	"github.com/lightstep/haproxy_log2span/platform"
@@ -39,11 +38,11 @@ func main() {
 		panic(err)
 	}
 
-	datadogClient, err := platform.SetupDatadog(applicationName, githash)
-	if err != nil {
-		rollbar.Error(rollbar.ERR, err)
-		panic(err)
-	}
+	// datadogClient, err := platform.SetupDatadog(applicationName, githash)
+	// if err != nil {
+	// 	rollbar.Error(rollbar.ERR, err)
+	// 	panic(err)
+	// }
 
 	t, err := tail.TailFile(flagFilename,
 		tail.Config{Follow: true,
@@ -54,17 +53,10 @@ func main() {
 		rollbar.Error(rollbar.ERR, err)
 		panic(fmt.Sprintf("Unable to open file: %v\n", err))
 	}
-
-	haproxyRegex := `^(?P<syslog_time>[^ ]* +[^ ]* +[^ ]*) (?P<syslog_host>[\w\-\.]+) (?P<ps>\w+)\[(?P<pid>\d+)\]: ((?P<c_ip>[\w\.]+):(?P<c_port>\d+) \[(?P<timestamp>.+)\] (?P<f_end>[\w\~\-]+) (?P<b_end>[\w\-]+)\/(?P<b_server>[\w\.\-]+) (?P<tq>\-?\d+)\/(?P<tw>\-?\d+)\/(?P<tc>\-?\d+)\/(?P<tr>\-?\d+)\/\+?(?P<tt>\d+) (?P<status_code>\d+) \+?(?P<bytes>\d+) (?P<req_cookie>\S?) (?P<res_cookie>\S?) (?P<t_state>[\w\-]+) (?P<actconn>\d+)\/(?P<feconn>\d+)\/(?P<beconn>\d+)\/(?P<srv_conn>\d+)\/\+?(?P<retries>\d+) (?P<srv_queue>\d+)\/(?P<backend_queue>\d+) (\{(?P<req_headers>[^}]*)\} )?(\{?(?P<res_headers>[^}]*)\}? )?"(?P<request>[^"]*)"?)`
-
-	var compiledRegex *regexp.Regexp
-	compiledRegex, err = regexp.Compile(haproxyRegex)
-	if err != nil {
-		rollbar.Error(rollbar.ERR, err)
-		panic(fmt.Sprintf("regexp returned error: %v\n", err.Error()))
-	}
-
 	for line := range t.Lines {
-		lib.ProcessLine(line.Text, compiledRegex, datadogClient)
+		err := lib.ProcessLine(line.Text, lib.DefaultHaproxyRegex)
+		if err != nil {
+			rollbar.Error(rollbar.ERR, err)
+		}
 	}
 }
--- lib/lib.go
+++ lib/lib.go
@@ -1,265 +1,303 @@
 package lib
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
-	"github.com/DataDog/datadog-go/statsd"
 	"github.com/lightstep/haproxy_log2span/network"
 	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/log"
-	"github.com/stvp/rollbar"
+	ot_log "github.com/opentracing/opentracing-go/log"
 )
 
+var timeZeroValue time.Time
+var (
+	HAProxyTimestamp    = "02/Jan/2006:15:04:05.999"
+	DefaultHaproxyRegex = regexp.MustCompile(`^(?P<syslog_time>[^ ]* +[^ ]* +[^ ]*) (?P<syslog_host>[\w\-\.]+) (?P<ps>\w+)\[(?P<pid>\d+)\]: ((?P<c_ip>[\w\.]+):(?P<c_port>\d+) \[(?P<timestamp>.+)\] (?P<f_end>[\w\~\-]+) (?P<b_end>[\w\-]+)\/(?P<b_server>[\w\.\-]+) (?P<tq>\-?\d+)\/(?P<tw>\-?\d+)\/(?P<tc>\-?\d+)\/(?P<tr>\-?\d+)\/\+?(?P<tt>\d+) (?P<status_code>\d+) \+?(?P<bytes>\d+) (?P<req_cookie>\S?) (?P<res_cookie>\S?) (?P<t_state>[\w\-]+) (?P<actconn>\d+)\/(?P<feconn>\d+)\/(?P<beconn>\d+)\/(?P<srv_conn>\d+)\/\+?(?P<retries>\d+) (?P<srv_queue>\d+)\/(?P<backend_queue>\d+) (\{(?P<req_headers>[^}]*)\} )?(\{?(?P<res_headers>[^}]*)\}? )?"(?P<request>[^"]*)"?)`)
+)
+
+// HAProxyHTTPLog represents a parsed HAProxy log line.
+// A more in detail description of the fields can be found here:
+// https://github.com/haproxy/haproxy/blob/master/doc/configuration.txt#L14991
+type HAProxyHTTPLog struct {
+	Request string
+	Retries int
+	// ReqHeaders contain any captured cookie headers.
+	ReqHeaders string
+	// StartTime is the exact time when the TCP connection was received by HAProxy
+	StartTime time.Time
+	// StatusCode is the HTTP status code returned to the client.
+	StatusCode int
+	// Frontend is the name of the frontend that received and processed the
+	// request.
+	Frontend string
+	// Backend is the name of the back that was selected to managed the connection
+	// to the server.
+	Backend string
+	// BackendServer is the name of the last server to which the connection
+	// was sent.
+	BackendServer string
+	// BackendQueue is the total number of requests which were processed before
+	// this one in the backend's global queue.
+	BackendQueue int
+	// SrvQueue is the total number of requests which were processed before
+	// this one in the server queue.
+	SrvQueue int
+	// BytesRead is the total number of bytes transmitted to the client.
+	BytesRead int
+	// TR is the total time in milliseconds spent waiting for a full HTTP request
+	// from the client (not counting body) after the first byte was received.
+	TR int
+	// Tw is the total time in milliseconds waiting in various queues.
+	Tw int
+	// Tc is the total time in milliseconds waiting for the server to send a full
+	// HTTP response, not counting data.
+	Tc int
+	// Tt is the total time in milliseconds elapsesd between the accept and the
+	// last close
+	Tt int
+	// Tq is the total time in milliseconds waiting for the client to send a full
+	// HTTP request, not counting data.
+	Tq int
+}
+
 // ProcessLine looks at a given line and creates a span with start and end times based on the durations and log timestamp.
-// It returns a err if there was an issue parsing or creating the span.
-func ProcessLine(line string, matchRegex *regexp.Regexp, datadogClient *statsd.Client) {
+// It returns an err if there was an issue parsing or creating the span.
+func ProcessLine(line string, matchRegex *regexp.Regexp) error {
 	match := matchRegex.FindStringSubmatch(line)
-	if match != nil {
-		var (
-			status_code   int
-			status_class  string
-			request       string
-			f_end         string
-			b_end         string
-			b_server      string
-			bytes         int
-			srv_queue     int
-			backend_queue int
-			tw            int
-			tt            int
-			tr            int
-			tq            int
-			startTime     time.Time
-			req_headers   string
-			tc            int
-			retries       int
+	if match == nil {
+		return fmt.Errorf("Could not match line with regex")
+	}
+	matches := make(map[string]string, matchRegex.NumSubexp())
+	for i, name := range matchRegex.SubexpNames() {
+		matches[name] = match[i]
+	}
+	logInfo, errs := newHaproxyLogFromMap(matches)
+	if len(errs) != 0 {
+		return fmt.Errorf("%v", errs)
+	}
+	return createSpans(logInfo)
+}
 
-			requestError    bool
-			queueError      bool
-			serverError     bool
-			clientError     bool
-			connectionError bool
-
-			err error
-		)
-
-		datadogClient.Count("log_matched", 1, nil, 1)
-
-		for i, name := range matchRegex.SubexpNames() {
-			switch name {
-			case "status_code":
-				status_code, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					status_code = -1
-					status_class = "unknown"
-				} else {
-					if status_code < 200 {
-						status_class = "1xx"
-					} else if status_code < 300 {
-						status_class = "2xx"
-					} else if status_code < 400 {
-						status_class = "3xx"
-					} else if status_code < 500 {
-						status_class = "4xx"
-					} else if status_code < 600 {
-						status_class = "5xx"
-					} else {
-						status_class = "unknown"
-					}
-				}
-			case "request":
-				request = match[i]
-			case "f_end":
-				f_end = match[i]
-			case "b_end":
-				b_end = match[i]
-			case "b_server":
-				b_server = match[i]
-			case "bytes":
-				bytes, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					bytes = -1
-				}
-			case "srv_queue":
-				srv_queue, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					srv_queue = -1
-				}
-			case "backend_queue":
-				backend_queue, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					backend_queue = -1
-				}
-			case "tw":
-				tw, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					tw = -1
-				}
-			case "tt":
-				tt, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					tt = -1
-				}
-			case "tr":
-				tr, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					tr = -1
-				}
-			case "tq":
-				tq, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					tq = -1
-				}
-			case "timestamp":
-				//2006-01-02T15:04:05.999999999Z07:00
-				haproxyTime := "02/Jan/2006:15:04:05.999"
-				startTime, err = time.Parse(haproxyTime, match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					datadogClient.Count("invalid_timestamp", 1, nil, 1)
-					return
-				}
-				startTime = startTime.Add(time.Duration(8) * time.Hour)
-			case "req_headers":
-				if strings.HasPrefix(match[i], "RQ") {
-					req_headers = match[i]
-
-				} else {
-					datadogClient.Count("headers_not_request_sid", 1, nil, 1)
-					return
-				}
-			case "tc":
-				tc, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					tc = -1
-				}
-			case "retries":
-				retries, err = strconv.Atoi(match[i])
-				if err != nil {
-					rollbar.Error(rollbar.ERR, err)
-					retries = -1
-				}
+func newHaproxyLogFromMap(matches map[string]string) (*HAProxyHTTPLog, []error) {
+	var errors []error
+	logInfo := &HAProxyHTTPLog{}
+	for name, val := range matches {
+		switch name {
+		case "status_code":
+			if statusCode, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.StatusCode = statusCode
+			}
+		case "request":
+			logInfo.Request = val
+		case "f_end":
+			logInfo.Frontend = val
+		case "b_end":
+			logInfo.Backend = val
+		case "b_server":
+			logInfo.Backend = val
+		case "bytes":
+			if bytes, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.BytesRead = bytes
+			}
+		case "srv_queue":
+			if srvQueue, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.SrvQueue = srvQueue
+			}
+		case "backend_queue":
+			if backendQueue, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.BackendQueue = backendQueue
+			}
+		case "tw":
+			if tw, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.Tw = tw
+			}
+		case "tt":
+			if tt, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.Tt = tt
+			}
+		case "tr":
+			if tr, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.TR = tr
+			}
+		case "tq":
+			if tq, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, err)
+			} else {
+				logInfo.Tq = tq
+			}
+		case "timestamp":
+			if startTime, err := time.Parse(HAProxyTimestamp, val); err != nil {
+				errors = append(errors, fmt.Errorf("Invalid timestamp %v (%v)", val, err))
+			} else {
+				// TODO: Remove hardcoded timezone correction
+				logInfo.StartTime = startTime.Add(time.Duration(8) * time.Hour)
+			}
+		case "req_headers":
+			logInfo.ReqHeaders = val
+		case "tc":
+			if tc, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, fmt.Errorf("Invalid timestamp %v (%v)", val, err))
+			} else {
+				logInfo.Tc = tc
+			}
+		case "retries":
+			if retries, err := strconv.Atoi(val); err != nil {
+				errors = append(errors, fmt.Errorf("Invalid timestamp %v (%v)", val, err))
+			} else {
+				logInfo.Retries = retries
 			}
 		}
+	}
+	return logInfo, errors
+}
 
-		if tw == -1 {
-			queueError = true
-			requestError = true
-			datadogClient.Count("set_queue_error", 1, nil, 1)
-		}
-
-		if tr == -1 {
-			serverError = true
-			requestError = true
-			datadogClient.Count("set_server_error", 1, nil, 1)
-		}
-
-		if tq == -1 {
-			clientError = true
-			requestError = true
-			datadogClient.Count("set_client_error", 1, nil, 1)
-		}
-
-		if tc == -1 {
-			connectionError = true
-			requestError = true
-			datadogClient.Count("set_connection_error", 1, nil, 1)
-		}
-
-		endTime := startTime.Add(time.Duration(tt) * time.Millisecond)
-
-		topSpan := opentracing.StartSpan(
-			"haproxy_request:"+b_end,
-			opentracing.StartTime(startTime))
-
-		topSpan.SetTag("http.status_code", status_code)
-		topSpan.SetTag("http.status_class", status_class)
-		// This needs to be filtered before it can be included.
-		// topSpan.SetTag("request", request)
-		_ = request
-		topSpan.SetTag("f_end", f_end)
-		topSpan.SetTag("b_end", b_end)
-		topSpan.SetTag("peer.ipv4", b_server)
-		topSpan.SetTag("peer.zone", network.GetZone(b_server))
-		topSpan.SetTag("bytes", bytes)
-		topSpan.SetTag("srv_queue", srv_queue)
-		topSpan.SetTag("backend_queue", backend_queue)
-		topSpan.SetTag("guid:request_sid", req_headers)
-		topSpan.SetTag("retries", retries)
-
-		if status_code >= 500 {
-			topSpan.SetTag("error", "true")
-		}
-
-		if requestError == true {
-			topSpan.SetTag("requestError", requestError)
-			topSpan.SetTag("queueError", queueError)
-			topSpan.SetTag("serverError", serverError)
-			topSpan.SetTag("requestError", requestError)
-			topSpan.SetTag("connectionError", connectionError)
-		}
-
-		if clientError != true {
-			clientTimeStart := startTime
-			clientTimeEnd := clientTimeStart.Add(time.Duration(tq) * time.Millisecond)
-			clientSpan := opentracing.StartSpan(
-				"client_read:"+b_end,
-				opentracing.StartTime(clientTimeStart),
-				opentracing.ChildOf(topSpan.Context()))
-
-			if queueError != true {
-				queueTimeStart := clientTimeEnd
-				queueTimeEnd := queueTimeStart.Add(time.Duration(tw) * time.Millisecond)
-				queueSpan := opentracing.StartSpan(
-					"queue_wait:"+b_end,
-					opentracing.StartTime(queueTimeStart),
-					opentracing.FollowsFrom(clientSpan.Context()))
-				if connectionError != true {
-					connectionTimeStart := queueTimeEnd
-					connectionTimeEnd := connectionTimeStart.Add(time.Duration(tc) * time.Millisecond)
-					connectionSpan := opentracing.StartSpan(
-						"connection:"+b_end,
-						opentracing.StartTime(connectionTimeStart),
-						opentracing.FollowsFrom(queueSpan.Context()))
-					if serverError != true {
-						responseTimeStart := connectionTimeEnd
-						headerTime := responseTimeStart.Add(time.Duration(tr) * time.Millisecond)
-						responseTimeEnd := endTime
-						serverSpan := opentracing.StartSpan(
-							"response:"+b_end,
-							opentracing.StartTime(responseTimeStart),
-							opentracing.FollowsFrom(connectionSpan.Context()))
-						serverSpan.FinishWithOptions(
-							opentracing.FinishOptions{
-								FinishTime: responseTimeEnd,
-								LogRecords: []opentracing.LogRecord{
-									opentracing.LogRecord{
-										Timestamp: headerTime,
-										Fields: []log.Field{
-											log.Int("header_millis", tr)}}}})
-					}
-					connectionSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: connectionTimeEnd})
-				}
-				queueSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: queueTimeEnd})
-			}
-			clientSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: clientTimeEnd})
-		}
-		topSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: endTime})
-	} else {
-		datadogClient.Count("log_not_matched", 1, nil, 1)
+func createSpans(log *HAProxyHTTPLog) error {
+	var (
+		requestError    bool
+		queueError      bool
+		serverError     bool
+		clientError     bool
+		connectionError bool
+	)
+	if log.StartTime == timeZeroValue {
+		return fmt.Errorf("Log does not contain a time value")
 	}
 
-	return
+	if log.Tw == -1 {
+		queueError = true
+		requestError = true
+	}
+
+	if log.TR == -1 {
+		serverError = true
+		requestError = true
+	}
+
+	if log.Tq == -1 {
+		clientError = true
+		requestError = true
+	}
+
+	if log.Tc == -1 {
+		connectionError = true
+		requestError = true
+	}
+
+	endTime := log.StartTime.Add(time.Duration(log.Tt) * time.Millisecond)
+
+	topSpan := opentracing.StartSpan(
+		"haproxy_request:"+log.Backend,
+		opentracing.StartTime(log.StartTime))
+
+	statusClass := parseStatusClass(log.StatusCode)
+	topSpan.SetTag("http.status_code", log.StatusCode)
+	topSpan.SetTag("http.status_class", statusClass)
+	// This needs to be filtered before it can be included.
+	// topSpan.SetTag("request", log.request)
+	topSpan.SetTag("f_end", log.Frontend)
+	topSpan.SetTag("b_end", log.Backend)
+	topSpan.SetTag("peer.ipv4", log.BackendServer)
+	topSpan.SetTag("peer.zone", network.GetZone(log.BackendServer))
+	topSpan.SetTag("bytes", log.BytesRead)
+	topSpan.SetTag("srv_queue", log.SrvQueue)
+	topSpan.SetTag("backend_queue", log.BackendQueue)
+
+	// TODO: provide a callback that allows you to set certain tags like this
+	topSpan.SetTag("guid:request_sid", log.ReqHeaders)
+	topSpan.SetTag("retries", log.Retries)
+
+	if log.StatusCode >= 500 {
+		topSpan.SetTag("error", "true")
+	}
+
+	if requestError == true {
+		topSpan.SetTag("requestError", requestError)
+		topSpan.SetTag("queueError", queueError)
+		topSpan.SetTag("serverError", serverError)
+		topSpan.SetTag("requestError", requestError)
+		topSpan.SetTag("connectionError", connectionError)
+	}
+
+	if clientError != true {
+		clientTimeStart := log.StartTime
+		clientTimeEnd := clientTimeStart.Add(time.Duration(log.Tq) * time.Millisecond)
+		clientSpan := opentracing.StartSpan(
+			"client_read:"+log.Backend,
+			opentracing.StartTime(clientTimeStart),
+			opentracing.ChildOf(topSpan.Context()))
+
+		if queueError != true {
+			queueTimeStart := clientTimeEnd
+			queueTimeEnd := queueTimeStart.Add(time.Duration(log.Tw) * time.Millisecond)
+			queueSpan := opentracing.StartSpan(
+				"queue_wait:"+log.Backend,
+				opentracing.StartTime(queueTimeStart),
+				opentracing.FollowsFrom(clientSpan.Context()))
+			if connectionError != true {
+				connectionTimeStart := queueTimeEnd
+				connectionTimeEnd := connectionTimeStart.Add(time.Duration(log.Tc) * time.Millisecond)
+				connectionSpan := opentracing.StartSpan(
+					"connection:"+log.Backend,
+					opentracing.StartTime(connectionTimeStart),
+					opentracing.FollowsFrom(queueSpan.Context()))
+				if serverError != true {
+					responseTimeStart := connectionTimeEnd
+					headerTime := responseTimeStart.Add(time.Duration(log.TR) * time.Millisecond)
+					responseTimeEnd := endTime
+					serverSpan := opentracing.StartSpan(
+						"response:"+log.Backend,
+						opentracing.StartTime(responseTimeStart),
+						opentracing.FollowsFrom(connectionSpan.Context()))
+					serverSpan.FinishWithOptions(
+						opentracing.FinishOptions{
+							FinishTime: responseTimeEnd,
+							LogRecords: []opentracing.LogRecord{
+								opentracing.LogRecord{
+									Timestamp: headerTime,
+									Fields: []ot_log.Field{
+										ot_log.Int("header_millis", log.TR)}}}})
+				}
+				connectionSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: connectionTimeEnd})
+			}
+			queueSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: queueTimeEnd})
+		}
+		clientSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: clientTimeEnd})
+	}
+	topSpan.FinishWithOptions(opentracing.FinishOptions{FinishTime: endTime})
+	return nil
+}
+
+// parseStatusCode takes an HTTP response code, parses it to an int, and returns
+// the code class it's in, e.g. 404 is a 4xx code.
+func parseStatusClass(code int) (class string) {
+	if code >= 100 && code < 200 {
+		class = "1xx"
+	} else if code < 300 {
+		class = "2xx"
+	} else if code < 400 {
+		class = "3xx"
+	} else if code < 500 {
+		class = "4xx"
+	} else if code < 600 {
+		class = "5xx"
+	} else {
+		class = "unknown"
+	}
+	return class
 }
