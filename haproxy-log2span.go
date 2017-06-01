fb72bec66fb53fbe73c730751cbe22d4d04f2d99
Brandon Gonzalez <bg@lightstep.com>
2017-06-01 10:59:35 -0400

    Removes twilio specific code

--- haproxy-log2span.go
+++ haproxy-log2span.go
@@ -7,10 +7,8 @@ import (
 	"time"
 
 	"github.com/lightstep/haproxy_log2span/lib"
-	"github.com/lightstep/haproxy_log2span/network"
-	"github.com/lightstep/haproxy_log2span/platform"
+	lightstep "github.com/lightstep/lightstep-tracer-go"
 	opentracing "github.com/opentracing/opentracing-go"
-	"github.com/stvp/rollbar"
 
 	"github.com/hpcloud/tail"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -25,33 +23,25 @@ func main() {
 
 	var (
 		flagFilename string
+		accessToken  string
 	)
 	kingpin.Flag("filename", "File to tail").Short('f').Required().StringVar(&flagFilename)
+	kingpin.Flag("access_token", "LS access token").Short('t').Required().StringVar(&accessToken)
 	kingpin.Parse()
 
 	fmt.Printf("haproxy-log2span.githash: %v\n", githash)
 
-	if err = platform.SetupRollbar(applicationName, githash); err != nil {
-		panic(err)
-	}
+	lightstepTracer := lightstep.NewTracer(lightstep.Options{
+		AccessToken: accessToken,
+	})
 
-	if err := platform.SetupOpentracing(); err != nil {
-		rollbar.Error(rollbar.ERR, err)
-		panic(err)
-	}
-
-	datadogClient, err := platform.SetupDatadog(applicationName, githash)
-	if err != nil {
-		rollbar.Error(rollbar.ERR, err)
-		panic(err)
-	}
+	opentracing.InitGlobalTracer(lightstepTracer)
 
 	parentSpanCB := func(sp opentracing.Span, log lib.HAProxyHTTPLog) error {
 		if !strings.HasPrefix(log.CapturedRequestHeaders, "RQ") {
 			return fmt.Errorf("No SID provided")
 		}
 		sp.SetTag("guid:request_sid", log.CapturedRequestHeaders)
-		sp.SetTag("peer.zone", network.GetZone(log.ServerName))
 		// This needs to be filtered before it can be included.
 		// topSpan.SetTag("request", log.request)
 		return lib.DefaultParentSpanCB(sp, log)
@@ -68,16 +58,11 @@ func main() {
 			MustExist: true,
 			Location:  &tail.SeekInfo{Offset: 0, Whence: os.SEEK_END}})
 	if err != nil {
-		rollbar.Error(rollbar.ERR, err)
 		panic(fmt.Sprintf("Unable to open file: %v\n", err))
 	}
 	for line := range t.Lines {
 		err := haproxyProcessor.Process(line.Text)
 		if err != nil {
-			rollbar.Error(rollbar.ERR, err)
-			datadogClient.Count("log_not_matched", 1, nil, 1)
-		} else {
-			datadogClient.Count("log_matched", 1, nil, 1)
 		}
 	}
 }
